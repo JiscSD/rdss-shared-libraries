aed538c97806d19785c9b68ff3f17fac93cdef76
Camilla Montonen <camilla@synbyote.io>
2017-10-17 16:34:37 +0100

    Revert "[RDSS-1512] Making sure that the boto Kinesis client is mocked by moto and removing unnecessary build env variables"
    This reverts commit 8800e7532a6ca306f9120ea1048f5b8905fbfce2.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,7 @@
 """Tests for Kinesis Client """
 
 import pytest
+import moto
 from moto import mock_kinesis
 from rdsslib.kinesis import client
 import json
@@ -8,6 +9,15 @@ import json
 
 class TestKinesisClient(object):
     """Test the Kinesis Client."""
+
+    def setup(self):
+        """Start mocking kinesis."""
+        self.mock = moto.mock_kinesis()
+        self.mock.start()
+
+    @pytest.fixture
+    def kc(self):
+        return client.KinesisClient()
 
     @pytest.fixture
     def payload(self):
@@ -27,11 +37,14 @@ class TestKinesisClient(object):
         return json.dumps(payload)
 
     @mock_kinesis
-    def test_write_and_read_messages(self, serialised_payload):
-        kc = client.KinesisClient()
+    def test_write_and_read_messages(self, serialised_payload, kc):
         kc.client.create_stream(StreamName='test_stream', ShardCount=2)
         kc.write_message(['test_stream'], serialised_payload, 1)
         messages = kc.read_messages('test_stream')
         sample_message = next(messages)
         assert sample_message['SequenceNumber'] == '1'
         assert json.loads(sample_message['Data'].decode('utf-8'))['messageBody'] == {'some' : 'message'}
+
+    def teardown(self):
+        """Stop mocking kinesis."""
+        self.mock.stop()
