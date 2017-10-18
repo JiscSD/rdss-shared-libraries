9878da91da4b5e80cf13b20f4bf5f15e79f8014e
Camilla Montonen <camilla@synbyote.io>
2017-10-18 17:47:39 +0100

    [RDSS-1512] Adding EnhancedKinesisClient to comply with functionality requirements in Content Based Router

--- rdsslib/kinesis/client.py
+++ rdsslib/kinesis/client.py
@@ -1,5 +1,7 @@
 import boto3
+import json
 import logging
+from .errors import MaxRetriesExceededException
 from .writer import StreamWriter
 from .reader import StreamReader
 
@@ -20,3 +22,31 @@ class KinesisClient(object):
     def read_messages(self, stream_name, seq_number=None):
         """Continuous loop that reads messages from stream_name"""
         return self.reader.read_stream(stream_name, seq_number=seq_number)
+
+
+class EnhancedKinesisClient(KinesisClient):
+    def __init__(self, decorator, error_handler):
+        super().__init__()
+        self.decorator = decorator
+        self.error_handler = error_handler
+
+    def _decorate_message_history(self, payload):
+        return self.decorator.process(payload)
+
+    def _check_payload_json_type(self, payload):
+        if type(json.loads(payload)) is not dict:
+            self.error_handler.handle_invalid_json(payload)
+
+    def write_message(self, stream_names, payload, max_attempts):
+        self._check_payload_json_type(payload)
+        decorated_payload = self._decorate_message_history(payload)
+        if decorated_payload:
+            try:
+                super().write_message(stream_names, payload, max_attempts)
+            except MaxRetriesExceededException as e:
+                stream_name = e.args[0]
+                error_code = 'GENERR005'
+                error_description = 'Maximum retry attempts [%s] exceed for stream [%s]' % (max_attempts, stream_name)
+                self.error_handler.handle_error(payload, error_code, error_description)
+        else:
+            self.error_handler.handle_invalid_json(payload)
