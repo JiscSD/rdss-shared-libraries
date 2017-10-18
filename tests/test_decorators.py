4d629ee8709f07010e6cfab4941f8d5828bed4d0
Camilla Montonen <camilla@synbyote.io>
2017-10-18 17:03:03 +0100

    [RDSS-1512] Adding Message history decorators

---
+++ tests/test_decorators.py
@@ -0,0 +1,33 @@
+import json
+import pytest
+from rdsslib.kinesis import decorators
+
+
+class TestHistoryDecorator(object):
+
+    def setup(self):
+        self.decorator = decorators.RouterHistoryDecorator()
+
+    @pytest.fixture
+    def payload(self):
+        """Return a sample payload."""
+        return {
+            'messageHeader': {
+                'id': '90cbdf86-6892-4bf9-845f-dbd61eb80065'
+            },
+            'messageBody': {
+                'some': 'message'
+            }
+        }
+
+    @pytest.fixture
+    def serialised_payload(self, payload):
+        """Return serialised payload"""
+        return json.dumps(payload)
+
+    def test_history_decorator(self, serialised_payload):
+        decorated_payload = self.decorator.process(serialised_payload)
+        json_payload = json.loads(decorated_payload)
+        msg_history = json_payload["messageHeader"]["messageHistory"][0]
+        for element in ["machineId", "timestamp", "machineAddress"]:
+            assert element in msg_history
