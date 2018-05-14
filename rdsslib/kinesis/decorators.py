10184e7a390bdd0ccbe33d41768c08b00bb2becf
Daniel Grant <daniel.grant@digirati.com>
2018-05-14 08:50:10 +0100

    [RDSS-2088] Fixed line length issues.

--- rdsslib/kinesis/decorators.py
+++ rdsslib/kinesis/decorators.py
@@ -23,18 +23,26 @@ class RouterHistoryDecorator(object):
         :return: decorated JSON formatted payload
         :rtype: str
         """
-        self.logger.info('Preparing to append history header to [%s] with machine ID [%s]', payload, machine_id)
+        self.logger.info(
+            'Preparing to append history header to [%s] with machine ID [%s]',
+            payload,
+            machine_id
+        )
         try:
             payload_json = json.loads(payload)
         except json.decoder.JSONDecodeError:
-            self.logger.exception('An error occurred decoding JSON payload [%s]', payload)
+            self.logger.exception(
+                'An error occurred decoding JSON payload [%s]',
+                payload
+            )
             payload_json = {}
 
         try:
             header = payload_json['messageHeader']
         except (TypeError, KeyError):
             self.logger.exception(
-                'An error occurred accessing [messageHeader] field on JSON payload [%s]',
+                'An error occurred accessing [messageHeader] field on '
+                'JSON payload [%s]',
                 payload_json
             )
             header = {}
@@ -43,7 +51,8 @@ class RouterHistoryDecorator(object):
             history = payload_json['messageHeader']['messageHistory']
         except (TypeError, KeyError):
             self.logger.exception(
-                'An error occurred accessing [messageHeader][messageHistory] field on JSON payload [%s]',
+                'An error occurred accessing [messageHeader][messageHistory] '
+                'field on JSON payload [%s]',
                 payload_json
             )
             history = []
@@ -54,7 +63,9 @@ class RouterHistoryDecorator(object):
             self.logger.info('Got machine name [%s]', machine_name)
         except (socket.gaierror, socket.herror):
             # Not sure what is best to express unknown: should pass validation
-            self.logger.info('Unable to retrieve machine name, using default [unknown]')
+            self.logger.info(
+                'Unable to retrieve machine name, using default [unknown]'
+            )
             machine_name = 'unknown'
 
         history_element = {
@@ -64,33 +75,50 @@ class RouterHistoryDecorator(object):
         }
 
         try:
-            self.logger.info('Appending history [%s] to existing message history [%s]', history_element, history)
+            self.logger.info(
+                'Appending history [%s] to existing message history [%s]',
+                history_element,
+                history
+            )
             history.append(history_element)
         except AttributeError:
             self.logger.exception(
-                'Unable to append [%s] to message history [%s], converting to array',
+                'Unable to append [%s] to message history [%s], converting'
+                ' to array',
                 history_element,
                 history
             )
             history = [history_element]
 
         try:
-            self.logger.info('Appending history header [%s] to existing message history [%s]', history_element, history)
+            self.logger.info('Appending history header [%s] to existing'
+                             ' message history [%s]', history_element, history)
             header['messageHistory'] = history
         except TypeError:
             self.logger.exception(
-                'Unable to append [%s] to message history [%s], fixing [messageHistory] element in [messageHeader]',
+                'Unable to append [%s] to message history [%s], fixing'
+                ' [messageHistory] element in [messageHeader]',
                 history_element,
                 history
             )
             header = {'messageHistory': history}
 
         try:
-            self.logger.info('Setting [messageHeader] on JSON payload [%s] to [%s]', payload_json, header)
+            self.logger.info(
+                'Setting [messageHeader] on JSON payload [%s] to [%s]',
+                payload_json,
+                header
+            )
             payload_json['messageHeader'] = header
         except TypeError:
-            self.logger.info('Unable to set [messageHeader] to [%s], fixing JSON payload', header)
+            self.logger.info(
+                'Unable to set [messageHeader] to [%s], fixing JSON payload',
+                header
+            )
             payload_json = {'messageHeader': header}
 
-        self.logger.info('Finished appending header, returning JSON payload [%s]', payload_json)
+        self.logger.info(
+            'Finished appending header, returning JSON payload [%s]',
+            payload_json
+        )
         return json.dumps(payload_json)
